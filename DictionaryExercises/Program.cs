5fce7821dffb2d48e00079358f8db187c8e61812
andrew <andrewjharris96@gmail.com>
2021-09-18 12:01:44 -0400

    done with dictionary

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,41 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+
+            // Get student names and grades
+            do
+            {
+                Console.WriteLine("Student: ");
+                string input = Console.ReadLine();
+                newStudent = input;
+
+                if (!Equals(newStudent, ""))
+                {
+                    Console.WriteLine("ID: ");
+                    input = Console.ReadLine();
+                    int idNumber = int.Parse(input);
+                    students.Add(idNumber, newStudent);
+
+                   
+                }
+
+            } while (!Equals(newStudent, ""));
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Value + " (" + student.Key + ")");
+               
+            }
+
+           
         }
     }
 }
