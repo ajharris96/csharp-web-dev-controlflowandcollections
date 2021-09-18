39f33e886c38e3ede2d55267595b16bdaa70d2f6
andrew <andrewjharris96@gmail.com>
2021-09-18 11:35:11 -0400

    arrays done

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,21 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+            int[] nums = new int[6]{ 1, 1, 2, 3, 5, 8 };
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Now only odds");
+            foreach (int num in nums)
+            {
+                if (num % 2 == 1)
+                {
+                    Console.WriteLine(num);
+                }
+               
+            }
+
         }
     }
 }
